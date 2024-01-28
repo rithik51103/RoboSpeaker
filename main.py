b168bda4b9d6c4a99915fa57f680cddbd217b906
Rithik Tripathi <126872911+rithik51103@users.noreply.github.com>
2024-01-29 00:12:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+
+# install Pyttsx3 library interpreter
+
+import win32com.client
+
+speaker = win32com.client.Dispatch("SAPI.SpVoice")
+
+while 1:
+    print("Enter the word you want to speak it out by computer")
+
+    s = input()
+    speaker.Speak(s)
